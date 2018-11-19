f94e4112f5fdf0de946e574b7178bb811fa1cee3
lenny <niele@corp.netease.com>
2018-11-19 21:11:35 +0800

    :bento: LiveBeansViewServlet 生成容器的快照

--- sample-console/src/main/java/com/dxy/lenny/App.java
+++ sample-console/src/main/java/com/dxy/lenny/App.java
@@ -2,6 +2,8 @@ package com.dxy.lenny;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,10 +11,10 @@ import com.opencsv.CSVWriter;
 
 /**
  * Hello world!
- *
  */
 public class App {
-	public static void main(String[] args) {
+
+  public static void main(String[] args) {
 
 //        Pattern p = Pattern.compile("java");
 //        Matcher m = p.matcher("java java java");
@@ -34,14 +36,37 @@ public class App {
 //
 //        System.out.println(m.replaceAll("c++"));
 
-		try (FileWriter writer = new FileWriter("/Users/niele/workplaces/temp/dddd.csv", true);
-		     CSVWriter csvWriter = new CSVWriter(writer)) {
-			writer.write('\uFEFF');
+//    try (FileWriter writer = new FileWriter("/Users/niele/workplaces/temp/dddd.csv", true);
+//        CSVWriter csvWriter = new CSVWriter(writer)) {
+//      writer.write('\uFEFF');
+//
+//      String[] arr = new String[]{"1231231231231231231231", "demo"};
+//      csvWriter.writeNext(arr);
+//    } catch (IOException e) {
+//    }
 
-			String[] arr = new String[]{"1231231231231231231231","demo"};
-			csvWriter.writeNext(arr);
-		}
-		catch (IOException e) {
-		}
-	}
+    String url = "https://www.dddd.com/asd?qdd=1212&asdd=22";
+
+    System.out.printf(String.valueOf(isValidUrl(url)));
+  }
+
+
+  public static boolean isValidUrl(String urlString) {
+    URI uri = null;
+    try {
+      uri = new URI(urlString);
+    } catch (URISyntaxException e) {
+      e.printStackTrace();
+      return false;
+    }
+
+    if (uri.getHost() == null) {
+      return false;
+    }
+    if (uri.getScheme().equalsIgnoreCase("http") || uri.getScheme().equalsIgnoreCase("https")) {
+      return true;
+    }
+    return false;
+  }
+
 }
