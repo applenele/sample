66530cda0708510291330795093cd248a110d0ab
applenele <1941973283@qq.com>
2018-11-30 10:03:19 +0800

    :heavy_plus_sign: 添加mybatis

---
+++ sample-springboot-mybatis/src/main/java/com/smallcode/sample/springboot/mybatis/SampleSpringbootMybatisApplication.java
@@ -0,0 +1,12 @@
+package com.smallcode.sample.springboot.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringbootMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringbootMybatisApplication.class, args);
+	}
+}
